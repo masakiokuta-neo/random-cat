2646586e78bdfef6be4f0b3ea6085d62f7766e93
masakiokuta-neo <masaki.okuta@neosystem.co.jp>
2026-04-16 18:32:21 +0900

    commit 1234567890abcdef1234567890abcdef12345678

--- app/cat-image.tsx
+++ app/cat-image.tsx
@@ -1,6 +1,6 @@
-"use client";
+"use client"; // (1) use clientを指定
  
-import { useState } from "react";
+import { useState } from "react"; // 追加
 import { fetchImage } from "./fetch-image";
 import styles from "./page.module.css"; // 追加
  
@@ -9,8 +9,10 @@ type CatImageProps = {
 };
  
 export function CatImage({ url }: CatImageProps) {
-  const [imageUrl, setImageUrl] = useState<string>(url);
+  // (2) useStateを使って状態を管理
+  const [imageUrl, setImageUrl] = useState(url);
  
+  // (3) 画像を取得する関数を定義
   const refreshImage = async () => {
     setImageUrl(""); // 初期化
     const image = await fetchImage();
